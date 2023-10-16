0e792da6027288403db92c4512fd6f79e13ac8b4
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 13:15:27 -0500

    Finalizada la clase Casa

--- Casa.java
+++ Casa.java
@@ -1,6 +1,14 @@
 
 package inmuebles;
 
-public class Casa {
-    
+public class Casa extends InmuebleVivienda {
+    protected int numeroPisos;
+    public Casa(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBanos, int numeroPisos){
+        super(identificadorInmobiliario, area, direccion, numeroHabitaciones, numeroBanos);
+        this.numeroPisos=numeroPisos;
+    }
+    void imprimir(){
+        super.imprimir();
+        System.out.println("Numero de pisos= "+numeroPisos);
+    }
 }
