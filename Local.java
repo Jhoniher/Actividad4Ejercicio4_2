a7849ba63860b324992c67dd25f380f6386e0e54
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 13:56:29 -0500

    Finalizada la clase Local

--- Local.java
+++ Local.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package inmuebles;
 
-/**
- *
- * @author Jhony
- */
-public class Local {
+public class Local extends Inmueble{
+    enum tipo {INTERNO, CALLE};
+    protected tipo tipoLocal;
     
+    public Local(int identificadorInmobiliario, int area, String direccion, tipo tipoLocal){
+        super(identificadorInmobiliario, area, direccion);
+        this.tipoLocal=tipoLocal;
+    }
+    void imprimir(){
+        super.imprimir();
+        System.out.println("Tipo de local= "+tipoLocal);
+    }
 }
