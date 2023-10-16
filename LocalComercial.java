c9b77cfe500711d543c5f9098d7a00604537aea3
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 14:00:02 -0500

    Finalizada clase LocalComercial

--- LocalComercial.java
+++ LocalComercial.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package inmuebles;
 
-/**
- *
- * @author Jhony
- */
-public class LocalComercial {
+public class LocalComercial extends Local {
+    protected static double valorArea=3000000;
+    protected String centroComercial;
     
+    public LocalComercial(int identificadorInmobiliario, int area, String direccion, tipo tipoLocal, String centroComercial){
+        super(identificadorInmobiliario, area, direccion, tipoLocal);
+        this.centroComercial=centroComercial;
+    }
+    void imprimir(){
+        super.imprimir();
+        System.out.println("Centro comercial="+centroComercial);
+        System.out.println();
+    }
 }
