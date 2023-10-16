fd4c86a956840e7a8e22300621cfe8a58a3827a9
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 13:28:49 -0500

    Finalizada la clase CasaUrbana

--- CasaUrbana.java
+++ CasaUrbana.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package inmuebles;
 
-/**
- *
- * @author Jhony
- */
-public class CasaUrbana {
-    
+public class CasaUrbana extends Casa{
+    public CasaUrbana(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBanos, int numeroPisos){
+        super(identificadorInmobiliario, area, direccion, numeroHabitaciones, numeroBanos, numeroPisos);
+    }
+    void imprimir(){
+        super.imprimir();
+    }
 }
